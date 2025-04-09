70164d246760916166456aa40516eb9303068214
Jim Lin <jylin@JimsdeMacBook-Air.local>
2025-04-09 22:22:38 +0800

    [Add] necessary code file

---
+++ hw1/gui.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel,
+    QLineEdit, QTextEdit, QGraphicsView, QGraphicsScene, QFileDialog
+)
+from PyQt5.QtGui import QPolygonF, QPen, QColor
+from PyQt5.QtCore import QPintF
+
+
