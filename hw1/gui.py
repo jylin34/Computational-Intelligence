5f1df3e9471ae69c8b0d4067f7d92feb67f81a94
Jim Lin <jylin@JimsdeMacBook-Air.local>
2025-04-09 22:50:09 +0800

    [Update] forget import, basic GUI created

--- hw1/gui.py
+++ hw1/gui.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QPolygonF, QPen, QColor
 from PyQt5.QtCore import QPointF
+from geometry import parse_track_file
 
 
 class TrackWindow(QWidget):
