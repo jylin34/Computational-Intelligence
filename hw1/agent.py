d50bda3a07970f808e989051b4e472414931ae93
Jim Lin <jylin@JimsdeMacBook-Air.local>
2025-04-11 20:57:29 +0800

    [Add] agent.py basic structure

---
+++ hw1/agent.py
@@ -0,0 +1,15 @@
+class Agent:
+    def __init__(self, lr, discount, epsilon, epsilon_decay, min_epsilon):
+        self.q_table = {}
+        self.lr = lr
+        self.discount = discount
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
+
+    def get_state(self, sensor_values):
+        
+    def select_action(self, state):
+
+    def update_q_table(self, state, action, reward, next_state):
+
